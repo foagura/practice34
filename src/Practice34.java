abb62790e333bf5e5e04bddc4c223ff66c37d383
foagura <foagura_0416@yahoo.co.jp>
2024-10-08 11:56:17 +0900

    first commit

---
+++ src/Practice34.java
@@ -0,0 +1,31 @@
+/* [参照型]の暗黙の型変換・キャストによる型変換 */
+
+abstract class Super {
+    int x; int y;
+    public abstract void print();
+    public void setNum(int x, int y) {
+        this.x = x; this.y = y;
+    }
+}
+
+class Sub extends Super {
+    public void print() {
+        System.out.println("xの値は：" + x + " " + "yの値は：" + y );
+    }
+
+    public void printS() {
+        System.out.println("このメソッドはSubC独自のメソッドです");
+    }
+}
+
+public class Practice34 {
+    public static void main(String[] args) {
+        Super objA = new Sub();    // 暗黙の型変換
+        objA.setNum(10, 20);
+        objA.print();
+
+        // objA.printS();    このままだとsuperにないメソッドを呼び出そうとしている為コンパイルエラーが出る
+        Sub objB = (Sub)objA;    // キャストによる型変換で元の型に戻す
+        objB.printS();    // 上記のキャストをしたことでSub独自のメソッドも呼び出せる
+    }
+}
